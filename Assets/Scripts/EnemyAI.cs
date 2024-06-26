f0d5d2bdd2f95d6292d2d10666256be287d5c810
anurupkoli <anurupkoli01@gmail.com>
2024-06-26 17:25:13 +0530

    New Zombie Shooter Game

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] float sensingRadius = 10f;
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    bool isProvoked;
+    float distance;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        isProvoked = false;
+        CalculateDistance();
+    }
+
+    void Update()
+    {
+        CalculateDistance();
+        CheckIsProvoked();
+        if(isProvoked){
+            MoveAgent();
+        }
+    }
+
+    void CalculateDistance(){
+        distance = Vector3.Distance(transform.position, target.position);
+    }
+
+    void CheckIsProvoked(){
+        if(distance <= sensingRadius){
+            isProvoked = true;
+        }
+    }
+
+    void MoveAgent(){
+        if(distance > navMeshAgent.stoppingDistance){
+            FollowTarget();
+        }
+        if(distance <= navMeshAgent.stoppingDistance){
+            AttackTarget();
+        }
+    }
+
+    void FollowTarget(){
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    void AttackTarget(){
+        Debug.Log("Attacking");
+    }
+
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, sensingRadius);
+    }
+}
